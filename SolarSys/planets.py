d3e26f3d549ae9139b60157f8abd16c6ddf189a1
Erich Hellemann <ehh@nmsu.edu>
2023-09-17 14:05:16 -0600

    added astropy

--- SolarSys/planets.py
+++ SolarSys/planets.py
@@ -1,32 +1,34 @@
-import units
 import numpy as np
+import pygame
+from astropy.constants import G, au
+from main import WinWidth, WinHeight
 
 def distance(point1, point2):
     # Calculate the difference vector between the points
     diff = point1 - point2
-    
     # Calculate the magnitude of the difference vector using the dot product and square root operations
     return np.sqrt(np.dot(diff, diff))
-    
-def gravitational_force(planet1, planet2):
-    G = 6.67430e-11  # Gravitational constant in N*m^2/kg^2
 
+
+def gravitational_force(planet1, planet2):
     # Calculate the distance between the two planets
     distance = ((planet1.x - planet2.x)**2 + (planet1.y - planet2.y)**2)**0.5
-
     # Calculate the gravitational force
-    force = G * (planet1.mass * planet2.mass) / (distance**2)
+    force = G.value * (planet1.mass * planet2.mass) / (distance**2)
 
     return force
-    
+
+   
 class Planet:
-    def __init__(self, name, x, y, mass, radius, velocity):
+    scale = 250 / au.value
+    def __init__(self, name, x, y, mass, radius, velocity, color):
         self.name = name
         self.x = x
         self.y = y
         self.mass = mass
         self.radius = radius
         self.velocity = velocity
+        self.color = color
 
     def __str__(self):
         return f"{self.name}: Planet at position ({self.x}, {self.y}) with mass {self.mass} kg and radius {self.radius} km"
@@ -40,3 +42,15 @@ class Planet:
         self.x = current_x + (self.x - current_x) + acceleration_x * (time_step**2)
         self.y = current_y + (self.y - current_y) + acceleration_y * (time_step**2)
 
+    def draw(self, win):
+        # update position to the center of the window and scale it 
+        x = self.x * self.scale + WinWidth / 2
+        y = self.y * self.scale + WinHeight / 2
+        pygame.draw.circle(win, self.color, (x, y), self.radius* self.scale)
+
+mercury = Planet("Mercury", x=57.909e9, y=0, mass=0.33010e24, radius=2440.5*1000, velocity=(0, 47.36*1000), color=(114, 47, 55))
+#venus = Planet("Venus", 0.72e11, 0, 4.8675e24, 6051.8, (0, 35020))
+#earth = Planet("Earth", 1.496e11, 0, 5.972e24, 6371, (0, 29783))
+#mars = Planet("Mars", 2.28e11, 0, 6.39e23, 3389.5, (0, 24007))
+
+CelestialObjects = [mercury]
---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from SolarSys.planets import *
+
+WinWidth = WinHeight = 900
+Window = pygame.display.set_mode((WinWidth, WinHeight))
+pygame.display.set_caption("Solar system simulation")
+
+
+if __name__ == "__main__":
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        Window.fill((0, 0, 0))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        for object in CelestialObjects:
+            object.draw(object)
+            
+    pygame.quit() 
+
